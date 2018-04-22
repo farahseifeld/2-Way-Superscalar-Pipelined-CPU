98968d75574c5b4cca0040a342bffae9eabbd44a
osama <osama.clueapps@gmail.com>
2018-04-22 18:32:18 +0200

    updating memory class

--- MIPSR4000/dmem.h
+++ MIPSR4000/dmem.h
@@ -6,9 +6,9 @@ class dmem
 public:
 	dmem();
 	~dmem();
-	void DF1();
-	void DF2(unsigned int address, unsigned int MEM_CT,unsigned int& data);
-	void TC();
+	void DF1(unsigned int& address_in, unsigned int& MEM_CT_in, unsigned int& data_wire);
+	void DF2(unsigned int& address_in, unsigned int& MEM_CT_in, unsigned int& data_wire);
+	void TC(unsigned int& address_in, unsigned int& MEM_CT_in, unsigned int& data_wire);
 private:
 	cache datacache;
 	unsigned int data;
@@ -20,33 +20,33 @@ dmem::dmem()
 dmem::~dmem()
 {
 }
-void dmem::DF1()
+void dmem::DF1(unsigned int& address_in, unsigned int& MEM_CT_in, unsigned int& data_wire)
 {
 	//NOTHING
 }
-void dmem::DF2(unsigned int address_in, unsigned int MEM_CT_in, unsigned int& data_in)
+void dmem::DF2(unsigned int& address_in, unsigned int& MEM_CT_in, unsigned int& data_wire)
 {
 	if (MEM_CT_in >> 3 == 0)
 	{
-		data_in = data;
+		data_wire = data;
 	}
 	else
 	{
 		if (MEM_CT_in == 0x00000002)//write
 		{
-			datacache.write(address_in, data_in);
-			data = data_in;
+			datacache.write(address_in, data_wire);
+			data = data_wire;
 		}
 		else if (MEM_CT_in == 0x00000002)//read
 		{
 			unsigned int tag;
 			int valid;
 			datacache.read(address_in, tag, data, valid);
-			data_in = data;
+			data_wire = data;
 		}
 	}
 }
-void dmem::TC()
+void dmem::TC(unsigned int& address_in, unsigned int& MEM_CT_in, unsigned int& data_wire)
 {
 	//NOTHING
 }
